2a4a27fbf468f622be9f6f432401986696807545
Nikolas Longen <nlongen@rptu.de>
2023-08-13 13:42:40 +0200

    Added usability features. init will now auto detect type of paramter integer no of qubits, path or json dump. Changed method names to be consitent in camelCase. This will break stuff for other packages....

--- src/qc_education_package/simulator.py
+++ src/qc_education_package/simulator.py
@@ -7,12 +7,13 @@
 import numpy as np
 from numpy.random  import rand
 import json
-
+import copy
+import os
 
 class Simulator():
 
 
-    def __init__(self, n=None, jsonDump=None, file=None, bitorder='big'):
+    def __init__(self, arg, n=None, json=None, file=None, bitorder='big'):
         """Constructor for quantum simulator. Creates simulator object. There a 3 setup methods.
         1) Provide n (first parameter) Simulator(n=3) or Simulator(3), create a simulator object with 3 qubits
         2) Restore a simulator object with a json dump Simulator(jsonDump=someDump)
@@ -25,17 +26,32 @@ class Simulator():
             littleEndian (str, optional): If 'big' bitorder big endian, if 'little' little endian, optional. Defaults to 'big'
         """
         # Standard bitorder big endian
+        self._precision = 3
         self._n = None
         if bitorder=='big':
-            self._bitOrder = -1
-        elif bitorder=='little':
             self._bitOrder = 1
+        elif bitorder=='little':
+            self._bitOrder = -1
         else:
             raise Exception("Unknown format for bitorder.")
 
-        # Prepare qubit base states
-        self._zero = np.array([[1],[0]])
-        self._one = np.array([[0],[1]])
+        # Prepare qubit base states now as needed to setup register
+        self._zero = np.array([[1],[0]], dtype=complex)
+        self._one = np.array([[0],[1]], dtype=complex)
+        
+        # Setup methods
+        if type(arg) == int:
+            self.reset(arg)
+        elif type(arg) == str:
+            if self.restoreFromFile(arg):
+                print(f"Restored simulator state from state file {arg}.")
+            elif self._restoreFromJsonDump(arg):
+                print("Restored simulator state from json dump.")
+            else:    
+                raise Exception("Wrong parameter string for Simulator, Pass either valid json string or path to a state file.")
+        else:
+            raise Exception("Wrong parameter type for Simulator, Pass either integer, valid json string or path to a state file.")
+        
         # Prepare one qubit gates/matrices
         self._I = np.identity(2)  # Identity in C2
         self._H = 1 / np.sqrt(2) * np.array([[1,1],[1,-1]])  # Hadamard gate
@@ -49,27 +65,16 @@ class Simulator():
         self._Rx = lambda phi : np.array([[np.cos(phi/2), -1j*np.sin(phi/2)],[-1j*np.sin(phi/2), np.cos(phi/2)]], dtype=complex) # Rotation about x-axis (Bloch Sphere)
         self._Ry = lambda phi : np.array([[np.cos(phi/2), -1*np.sin(phi/2)],[np.sin(phi/2), np.cos(phi/2),]], dtype=complex) # Rotation about y-axis (Bloch Sphere)
         self._Rz = lambda phi : np.array([[np.exp(-1j*phi/2), 0],[0, np.exp(1j*phi/2)]], dtype=complex) # Rotation about z-axis (Bloch Sphere)
-
-        # Setup methods
-        if jsonDump is not None:
-            self._restoreFromJsonDump(jsonDump)
-        elif file is not None:
-            self.restoreFromFile(file)
-        else:
-            assert(type(n)==int)
-            self.reset(n)
         
 
     # Overrides
     def __str__(self):
-        """Overrides objects toString to export simulator in json format.
+        """Overrides objects toString and returns formatted string of current register state.
 
         Returns:
             str: n and register amplitudes and phases in json format.
         """
-        amp = np.abs(self._register).flatten().tolist()
-        phase = np.angle(self._register).flatten().tolist()
-        return json.dumps({'n': self._n, 'amp': amp, 'phase': phase, 'bitorder': self._bitOrder})
+        return self.format(self._register.reshape((2**self._n, 1))) + "\n"
 
 
     def __eq__(self, o):
@@ -84,51 +89,103 @@ class Simulator():
         if o is not None:
             if o._bitOrder == self._bitOrder:
                 if isinstance(o, Simulator):
-                    return np.array_equal(self._register, o._register)
+                    # Round to account for numeric errors
+                    return np.array_equal(np.round(self._register,self._precision), np.round(o._register,self._precision))
             else:
                 print("Can't compare due to different (qu)bitorder.")
         return False 
 
 
+    def toJson(self):
+        """To export simulator in json format.
+
+        Returns:
+            str: n and register amplitudes and phases in json format.
+        """
+        amp = np.abs(self._register).flatten().tolist()
+        phase = np.angle(self._register).flatten().tolist()
+        # amp = np.around(amp, decimals=self._precision)  #
+        # phase = np.around(phase, decimals=self._precision)
+        return json.dumps({'n': self._n, 'amp': amp, 'phase': phase, 'bitorder': self._bitOrder, 'precision': self._precision})
+
+
+    def setPrecision(pre):
+        self._precision = pre
+
+
+    def format(self, arr: np.array, pre=-1) -> str:
+        if np.all(np.imag(arr)< 1e-6):  
+            arr = np.real(arr)  # if imag part of all comp is zero, only show real part
+        return np.array2string(arr, precision=self._precision, suppress_small=True , sign=' ')
+
+
+    def print(self, arr: np.array, pre=-1):
+        print(self.format(arr, pre) + "\n") 
+
+
+    def copy(self) -> 'Simulator':
+        return copy.copy(self)
+
+
     # export/import
-    def export_state(self, name = 'simulator', path=''):  
+    def exportState(self, fname = 'simulator.state'):  
         """Export current simulator state to file.
 
         Args:
             name (str, optional): Name for the export. Defaults to 'simulator'.
             path (str, optional): Path to store at, end with '/' resp. '\\'. Defaults to '' -> working directory.
         """
-        fname = f"{name}.state" if path == '' else f"{path}{name}.state"  # Not nice but avoids handling os specific path shenanigans
+        # fname = f"{name}" if path == '' else f"{path}{name}"  # Not nice but avoids handling os specific path shenanigans
         with open(fname, "w") as text_file:
-            text_file.write(str(self))
+            text_file.write(self.toJson())
             text_file.close()
 
 
-    def restoreFromFile(self, path):
+    def restoreFromFile(self, path) -> bool:
         """Restore simulator state from previously exported state
 
         Args:
-            fname (str): relative or absolute path to file
+            fname (str): relative or absolute path to file.
+
+        Returns:
+            bool: True if success, False if not.
         """
-        with open(path, "r") as text_file:
-            self._restoreFromJsonDump(text_file.read())
-            text_file.close
+        if os.path.exists(path):
+            try:
+                with open(path, "r") as text_file:
+                    self._restoreFromJsonDump(text_file.read())
+                    text_file.close
+            except:
+                return False
+            finally:
+                return True
+        else:
+            return False
 
-
-    def _restoreFromJsonDump(self, jsonDump):
+    def _restoreFromJsonDump(self, jsonDump) -> bool:
         """Restore simulator state from generated json dump. 
         Dumps can be created using str(SimulatorObject)
 
         Args:
             jsonDump (str): json Dump of Simulator object
+
+        Returns:
+            bool: True is success, False is error
         """
-        state = json.loads(jsonDump)
-        self._n = int(state['n'])
-        self._Ib = np.identity(2**self._n)  # Identity in comp. base
-        self._bitOrder = state['bitorder']
-        amp = np.array(state['amp'])
-        phase = np.array(state['phase'])
-        self._register = amp*np.exp(1j*phase)  # export was normed, no need to check here
+        try:
+            state = json.loads(jsonDump)
+            self._n = int(state['n'])
+            self._Ib = np.identity(2**self._n)  # Identity in comp. base
+            self._bitOrder = state['bitorder']
+            self._precision = int(state['precision'])
+            amp = np.array(state['amp'])
+            phase = np.array(state['phase'])
+            self._register = amp*np.exp(1j*phase)  # export was normed, no need to check here
+            self._register.flatten()
+        except:
+            return False
+        finally:
+            return True
 
 
     # Preparing state
@@ -142,10 +199,10 @@ class Simulator():
         if n is not None and n != self._n:
             self._n = n
             self._Ib = np.identity(2**n)  # Identity in comp. base
-        self.write_integer(0)
+        self.writeInteger(0)
 
    
-    def write_integer(self, val, qubit=1):  
+    def writeInteger(self, val, qubit=1):  
         """Write given integer value in binary. Attention! This will overwrite the register. Use this only to prepare your qubit/register.
 
         Args:
@@ -170,10 +227,10 @@ class Simulator():
             val (float or list): State is build to (1-p)|0> + p|1>. Hence p=0 -> |0>; p=1 -> |1>
             qubit (int or list, optional): qubit(s) to be set. Defaults to None.
         """
-        self.write_integer(val, qubit)
+        self.writeInteger(val, qubit)
 
 
-    def write_complex(self, a):
+    def writeComplex(self, a):
         """Prepare register with given complex amplitudes e.g. reg = a0 |0> + a1 |1>
         Attention! This will override the register. Use this only to prepare your qubit/register.
 
@@ -191,8 +248,7 @@ class Simulator():
         self._register = a
 
 
-
-    def write_magn_phase(self, magnitude, phase):
+    def writeMagnPhase(self, magnitude, phase):
         """Prepare register with given amplitudes and phases e.g. reg = amp0 exp(i phase0) |0> + amp1 exp(i phase1) |1>
         Attention! This will override the register. Use this only to prepare your qubit/register.
 
@@ -205,16 +261,19 @@ class Simulator():
         magnitude = np.array(magnitude)
         phase = np.array(phase)
         out = magnitude*np.exp(1j * np.deg2rad(phase))
-        self.write_complex(out)
+        self.writeComplex(out)
 
 
-    def read(self, qubit=None, basis='c') -> int:
-        """Read given qubit. If no qubit is given (qubit=None) all qubit are measured.
+    def read(self, qubit=None, size=None, basis='c', stdout=True) -> int:
+        """Read given qubit. If no qubit is given (qubit=None) all qubit are measured. If a size parameter is provided, the measurement will be repeated returning the measurement
+        statistics. Here the simulator state will not collaps after the independent measurements.
 
         Args:
             qubit (int or list(int), optional): qubit to read. Defaults to None.
+            size (int), optional): Repeat the measurement size times. Default 1 Measurement.
             basis (char or np.array, optional): Basis in which measurement ist performed, Either char c: comp. basis; y,x or z: bell basis; h: had; defaults to c 
                                                 OR 2x2 np.array to pass custom projector. 
+            stdout (bool): Wheter to print measurement results in stdout (Default True)
 
         Returns:
             np.array: Probabilities for for register/single qubit.
@@ -234,17 +293,23 @@ class Simulator():
             print("Using custom projector for measurement.")
             # NOTE: expecting 2x2 single qubit operation here.
             proj = basis
-
+        msg = ""
+        result = 0
         if qubit is None:
             # Measure all qubits
             if basis != 'c':
                 # project all qubit with given projector, POVM Measurement
                 self._operatorInBase(proj)
             prop = np.square(np.abs(self._register)).flatten()
-            result = np.random.choice(a=2**self._n, p=prop) 
-            self.write_integer(result)
-            out = np.binary_repr(result).zfill(self._n)
-            msg = f"Measured state |{out}>."
+            result = np.random.choice(a=2**self._n, p=prop, size=size) 
+            # TODO size is None unterscheidung
+            if size is None:
+                self.writeInteger(result)
+                out = np.binary_repr(result).zfill(self._n)
+                msg = f"Measured state |{out}>."
+            else:
+                # For repeated measurement the state will not collaps
+                msg = f"Measured {size:d} times."
 
 
         elif type(qubit) == int: # Measuring one qubit
@@ -270,25 +335,35 @@ class Simulator():
             # Check if state was normed
             assert(1 - p0 - p1 < 1e-6)
             # Project to new state
-            result = np.random.choice(a=[0,1], p=[p0**2, p1**2])
-            state = state1 if result else state0 
-            norm = p1 if result else p0
-            # Normalize state
-            self._register = state / norm
-            msg =  f"Measurement qubit {qubit:2d}: |{result:d}> \t (|0>: {p0**2:2.2%} |1>: {p1**2:2.2%})."
+            result = np.random.choice(a=[0,1], p=[p0**2, p1**2], size=size)
+            if size is None:
+                # For repeated measurement the state will not collaps
+                state = state1 if result else state0 
+                norm = p1 if result else p0
+                # Normalize state
+                self._register = state / norm
+                msg =  f"Measurement qubit {qubit:2d}: |{result:d}>  (|0>: {p0**2:2.2%} |1>: {p1**2:2.2%})."
 
 
         elif type(qubit) == list:
+            # Independent probabilities
+            result = np.zeros((len(qubit),1)) if size is None else np.zeros((len(qubit), size))
             msg = ""
-            for qb in qubit:
-                msg += f"{self.read(qb)}\n"
+            for i in  range(len(qubit)):
+                text, result[i,:] = self.read(qubit[i], stdout=False, size=size)
+                msg += f"{text}\n"
+            if size is not None:
+                msg =  f"Measured qubits {qubit} {size} times.\n"
 
 
         else:
+            print(type(qubit))
             raise Exception('Qubit has wrong type. Pass int or list of int or None')
-           
-        print(msg)
-        return msg
+        
+        if stdout:
+            print(msg)
+
+        return msg, result
 
 
     #Set global Phase 0
@@ -681,7 +756,7 @@ class Simulator():
         """
         result = 1
         for i in ops_to_kron[::self._bitOrder]:               
-            result = np.kron(i, result)     
+            result = np.kron(result, i)     
         return result
         # Can be shorter using functools.reduce() 
         # See https://docs.python.org/3/library/functools.html#functools.reduce
@@ -726,11 +801,9 @@ class Simulator():
             np.array: Matrix for controlled operator in comp. basis.
         """
         # bitorder
-        print(control_qubit, target_qubit, self._n)
         control_qubit = np.array(control_qubit, dtype=int)
         control_qubit = self._n-control_qubit if self._bitOrder else control_qubit-1  # for correct bitorder little/big endian
         target_qubit = self._n-target_qubit if self._bitOrder else target_qubit-1  # for correct bitorder little/big endian
-        print(control_qubit, target_qubit, self._n)
         # assert(target_qubit >= 0 and target_qubit < self._n)
         # assert(np.all(control_qubit >= 0) and np.all(control_qubit < self._n))
         # assert(np.all(target_qubit != control_qubit))
@@ -748,5 +821,3 @@ class Simulator():
         op = control0 + control1
         self._register = op @ self._register
         return op
-
-# TODO: printnice methoud to output matrices. Threshold value such that noise gets zeroed out eg val < 1e15 -> 0
